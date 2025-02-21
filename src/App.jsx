178620172d7c1169d7f4be90cee993b1ba6fe072
Prince Shrivastava <prince9b407@gmail.com>
2025-02-21 18:14:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <h1 className='font-bold text-5xl text-center bg-purple-200'>Hii Swipe Recruit</h1>
+    </div>
+  )
+}
+
+export default App
