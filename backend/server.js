f5f73b54008333c2690580e4ee2b35e1817bc1e9
Prince Raj <91085200+shrivastava-26@users.noreply.github.com>
2025-03-13 00:35:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,8 @@ let dbStart = async() => {
         await connectToDb()
         console.log("db connceted successfully")
 
-        app.listen(process.env.PORT || 8080, () => {
-          console.log(`Server is started on port: ${process.env.PORT || 8080}`);
+        app.listen(process.env.PORT, () => {
+          console.log(`Server is started on port: ${process.env.PORT}`);
         });
         
     } catch (error) {
